9986cdffc670bbbda3861ca67b0dc60024d216cc
jmacey <jmacey@bournemouth.ac.uk>
2022-11-03 21:24:38 +0000

    updated for NGL9 and renamed master branch main

--- src/MatrixStack.cpp
+++ src/MatrixStack.cpp
@@ -5,56 +5,51 @@
 
 void MatrixStack::pushMatrix()
 {
-  if(++m_top< m_depth)
+  if (++m_top < m_depth)
   {
     // copy the matrix to the top of stack
-    m_stack[m_top]=m_stack[m_top-1];
+    m_stack[m_top] = m_stack[m_top - 1];
   }
   else
   {
-    std::cerr<<"Matrix stack overflow\n";
+    std::cerr << "Matrix stack overflow\n";
     exit(EXIT_FAILURE);
   }
 }
 void MatrixStack::popMatrix()
 {
   m_stack[m_top].identity();
-  if(m_top--<=0 )
+  if (m_top-- <= 0)
   {
-    std::cerr<<"Matrix stack underflow \n";
+    std::cerr << "Matrix stack underflow \n";
     exit(EXIT_FAILURE);
   }
 }
 
-
 void MatrixStack::rotate(float _x, float _y, float _z)
 {
-  ngl::Mat4 x,y,z;
-  x.rotateX(_x);
-  y.rotateY(_y);
-  z.rotateZ(_z);
-  ngl::Mat4 final=z*y*x;
-  m_stack[m_top]*=final;
+
+  auto x = ngl::Mat4::rotateX(_x);
+  auto y = ngl::Mat4::rotateY(_y);
+  auto z = ngl::Mat4::rotateZ(_z);
+  ngl::Mat4 final = z * y * x;
+  m_stack[m_top] *= final;
 }
 
-void MatrixStack::rotate(float _angle,float _x, float _y, float _z)
+void MatrixStack::rotate(float _angle, float _x, float _y, float _z)
 {
-  ngl::Mat4 r;
-  r.euler(_angle,_x,_y,_z);
-  m_stack[m_top]*=r;
+  auto r = ngl::Mat4::euler(_angle, _x, _y, _z);
+  m_stack[m_top] *= r;
 }
 
 void MatrixStack::translate(float _x, float _y, float _z)
 {
-  ngl::Mat4 t;
-  t.translate(_x,_y,_z);
-  m_stack[m_top]*=t;
+  auto t = ngl::Mat4::translate(_x, _y, _z);
+  m_stack[m_top] *= t;
 }
 
 void MatrixStack::scale(float _x, float _y, float _z)
 {
-  ngl::Mat4 s;
-  s.scale(_x,_y,_z);
-  m_stack[m_top]*=s;
+  auto s = ngl::Mat4::scale(_x, _y, _z);
+  m_stack[m_top] *= s;
 }
-
